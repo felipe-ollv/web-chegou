5832df48b81e1a494fd793cf76de8cfe2e2d3d32
Felipe Oliveira <felipeoliveira264@gmail.com>
2025-11-12 20:52:52 -0300

    estrutura inicial do projeto

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import PdfPage from "./pages/PdfPage";
 
-function App() {
+export default function App() {
+  const isAuth = !!localStorage.getItem("authToken");
 
   return (
-    <div>
-      Inicio
-    </div>
-  )
+    <BrowserRouter basename="/painel">
+      <Routes>
+        <Route path="/" element={isAuth ? <Navigate to="/dashboard" /> : <LoginPage />} />
+        <Route path="/dashboard" element={isAuth ? <DashboardPage /> : <Navigate to="/" />} />
+        <Route path="/pdf" element={isAuth ? <PdfPage /> : <Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
