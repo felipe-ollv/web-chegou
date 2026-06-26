8f2c8a5af306abdeef47214aabb89452c14fa224
Felipe Oliveira <felipeoliveira264@gmail.com>
2026-06-26 18:22:14 -0300

    novo build, pasta dist

--- src/utils/execute-in-time.ts
+++ src/utils/execute-in-time.ts
@@ -3,6 +3,7 @@ export class ExecuteInTime {
 
   static execute(callback: () => void, time: number): () => void {
     const intervalId = setInterval(callback, time);
+
     return () => clearInterval(intervalId);
   }
 }
